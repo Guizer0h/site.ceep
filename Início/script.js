24043ac03c43277152fac55f31ae3a7bf7664da4
Guizer0h <hara.guilherme@escola.pr.gov.br>
2026-09-02 23:32:37 -0300

    Adição da aba adm

---
+++ Início/script.js
@@ -0,0 +1,48 @@
+window.addEventListener("scroll", function () {
+    const header = document.getElementById("main-header");
+    if (window.scrollY > 400) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
+
+const logo = document.querySelector('.logo-animada');
+
+window.addEventListener('scroll', () => {
+    const posicaoLogo = logo.getBoundingClientRect().top;
+    const alturaTela = window.innerHeight;
+
+    if (posicaoLogo < alturaTela - 100) {
+        logo.classList.add('visivel');
+    }
+
+});
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+
+    const clicouNoMenu = navMenu.contains(e.target);
+    const clicouNoHamburger = hamburger.contains(e.target);
+
+    if (!clicouNoMenu && !clicouNoHamburger) {
+        navMenu.classList.remove("active");
+    }
+
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
